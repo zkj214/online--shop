93cabe447cda24125fe20e35e746603247c713f5
Z-Jix <pythonexercise214@gmail.com>
2025-06-11 16:36:13 +0800

    Add a STATIC_ROOT value

--- ecom/settings.py
+++ ecom/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[STATIC_DIR]
-STATIC_ROOT==""
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=MEDIA_DIR
