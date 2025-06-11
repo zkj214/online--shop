e8baee3f0e1cace5329d821653bd2ad52eac6c4b
Z-Jix <pythonexercise214@gmail.com>
2025-06-11 16:53:13 +0800

    Updata the build

--- build.sh
+++ build.sh
@@ -10,3 +10,10 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
--- ecom/settings.py
+++ ecom/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[STATIC_DIR]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #required on render
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=MEDIA_DIR
